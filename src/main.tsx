fa8351bd39ec148427c042fc13b7986589b95185
[dyad] <git@dyad.sh>
2025-06-29 19:59:56 +0200

    [dyad] Suppression du contournement CSP pour résoudre l'erreur DOM - wrote 1 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import "./utils/cspWorkaround"; // Import the new script
+// Removed: import "./utils/cspWorkaround";
 
 createRoot(document.getElementById("root")!).render(<App />);
