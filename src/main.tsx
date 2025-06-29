df29fc12722d76d944b43f325432eafb6b54669f
[dyad] <git@dyad.sh>
2025-06-29 19:53:42 +0200

    [dyad] Correction des problèmes de politique de sécurité du contenu (CSP) - wrote 3 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import "./utils/cspWorkaround"; // Import the new script
 
 createRoot(document.getElementById("root")!).render(<App />);
